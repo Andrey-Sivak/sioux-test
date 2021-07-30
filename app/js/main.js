e30f79c96df02b8759a538a6b19a2d604656a4c9
a.sivak <riziy_bes@mail.ru>
2021-07-30 15:32:41 +0300

    init

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+import * as $ from 'jquery';
+import './jquery.validate.min.js';
+import './jquery.inputmask.min';
+import './slick.min';
+import './select2.min';
+
+window.addEventListener('load', function () {
+
+    console.log(12);
+});
