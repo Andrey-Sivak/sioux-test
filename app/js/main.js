07be5bbd165e09f0d0f857db033bc9d495e7e292
a.sivak <riziy_bes@mail.ru>
2021-08-03 02:22:52 +0300

    created all authors page

--- app/js/main.js
+++ app/js/main.js
@@ -6,5 +6,18 @@ import './slick.min';
 
 window.addEventListener('load', function () {
 
+    (function loader() {
+        if (!document.querySelector('.loader')) {
+            return;
+        }
 
+        const loader = document.querySelector('.loader');
+        if (loader.classList.contains('active')) {
+            loader.classList.remove('active');
+
+            setTimeout(() => {
+                loader.parentElement.removeChild(loader);
+            }, 300)
+        }
+    })();
 });
