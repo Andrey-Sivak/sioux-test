a35bdf2237c6ecffb39195b5c6f6a9cce1883552
a.sivak <riziy_bes@mail.ru>
2021-08-03 17:06:29 +0300

    some editions

--- app/js/main.js
+++ app/js/main.js
@@ -20,4 +20,42 @@ window.addEventListener('load', function () {
             }, 300)
         }
     })();
+
+    (function form() {
+        if (!document.querySelector('form')) {
+            return;
+        }
+
+        const form = document.querySelector('form');
+        const textarea = form.querySelector('textarea');
+        textarea.addEventListener('input', textareaHandleInput);
+
+        function textareaHandleInput(e) {
+            if (this.scrollHeight > this.clientHeight) {
+                this.style.height = this.scrollHeight + 'px';
+            }
+        }
+    })();
+
+    (function scrollToForm() {
+        if (!document.querySelector('form')) {
+            return;
+        }
+
+        const btn = [...document.querySelectorAll('.menu__item_link')]
+            .filter(d => d.innerHTML === 'контакты')[0];
+
+        btn.addEventListener('click', scroll);
+
+        function scroll(e) {
+            e.preventDefault();
+            const form = document.querySelector('form');
+            const formTop = form.getBoundingClientRect().top + pageYOffset;
+
+            window.scrollTo({
+                top: formTop,
+                behavior: 'smooth'
+            });
+        }
+    })();
 });
